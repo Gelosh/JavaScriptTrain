6222f158416aabce4acdeec380989d8a5884fb7b
Oleg <gelosh82@gmail.com>
2023-01-17 15:00:38 +0300

    hw 8

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,36 @@
 'use strict'
+let btn = document.getElementById("start"),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
 
-let money = +prompt("Ваш бюджет на месяц?", ""),
+    inputPole = document.getElementsByClassName("expenses-item"),
+    expensesBtn = document.getElementsByTagName('button')[0],
+    btnConfirm = document.getElementsByTagName("button")[1],
+    btnCount = document.getElementsByTagName("button")[2],
+    fieldOption = document.querySelectorAll(".optionalexpenses-item"),
+    incomeItem = document.querySelector('.choose-income'),
+	checkSavings = document.querySelector('#savings'),
+	sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+let money, date;
+function start() {   
+    money = +prompt("Ваш бюджет на месяц?", ""),
     date = prompt("Введите дату в формате YYYY-MM-DD", "1985-06-22");
 
     while(isNaN(money) || money == "" || money == null) {
         money = +prompt("Ваш бюджет на месяц?", "");
     }
-
+}
 start();
 
 let appData = {
